ad58af9a3833fbb0f8c982839bf1ddc383bd026d
Noel Madali <adelbert.madali@altisource.com>
2015-02-25 17:02:02 -0500

    Added config object for css assets and task to copy css assets

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,15 @@ var del = require('del');
 var sass = require('gulp-sass');
 var express = require('express');
 
+// config of files
+var wcConfig = {
+	assets: {
+		css: [
+			'bower_components/normalize.css/normalize.css'
+		]
+	}
+};
+
 // clean dist
 gulp.task('clean', function (cb) {
 	del('dist', cb);
@@ -12,6 +21,12 @@ gulp.task('clean', function (cb) {
 gulp.task('scss', function () {
 	gulp.src('scss/styles.scss')
 		.pipe(sass())
+		.pipe(gulp.dest('dist/css'));
+});
+
+// copy css assets
+gulp.task('copy:css', function () {
+	gulp.src(wcConfig.assets.css)
 		.pipe(gulp.dest('dist/css'));
 });
 
